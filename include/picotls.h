21926015f52876921c890f2c173593e9b5231de7
sus0pid <s2136500@ed.ac.uk>
2024-04-03 06:00:47 +0100

    step in send_client_hello()

--- include/picotls.h
+++ include/picotls.h
@@ -2066,6 +2066,9 @@ inline void ptls_hash_clone_memcpy(void *dst, const void *src, size_t size)
         init_func(&ctx->ctx);                                                                                                      \
         return &ctx->super;                                                                                                        \
     }
+extern int decode_session_identifier(uint64_t *issued_at, ptls_iovec_t *psk, uint32_t *ticket_age_add, ptls_iovec_t *ticket_ctx,
+                                     uint16_t *key_exchange_id, uint16_t *csid, ptls_iovec_t *negotiated_protocol, const uint8_t *src,
+                                     const uint8_t *const end);
 
 #ifdef __cplusplus
 }
