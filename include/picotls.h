25694b233f4f93566da100238df98dc3b1dec7b0
sus0pid <s2136500@ed.ac.uk>
2024-04-03 14:23:04 +0100

    add todo of decode_sdp_ticket() in send_client_hello

--- include/picotls.h
+++ include/picotls.h
@@ -1034,7 +1034,7 @@ typedef struct st_ptls_handshake_properties_t {
             /**
              * indicate if the session_ticket is sdpticket @xinshu
              */
-             size_t use_sdp_ticket;
+             unsigned use_sdp_ticket: 0;
             /**
              * pointer to store the maximum size of early-data that can be sent immediately. If set to non-NULL, the first call to
              * ptls_handshake (or ptls_handle_message) will set `*max_early_data` to the value obtained from the session ticket, or
